d8dbb1c8d9c4bfde4982034ef53e59de24196e75
Michelli Prieto <Michellifernanda.fp@gmail.com>
2024-01-31 18:13:28 +0100

    ok

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Cliente;
 use Illuminate\Http\Request;
 
 class ClienteController extends Controller
@@ -11,7 +11,7 @@ class ClienteController extends Controller
      */
     public function index()
     {
-        //
+        return Cliente::all();
     }
 
     /**
@@ -19,8 +19,22 @@ class ClienteController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'nombre' => 'required',
+            'correo' => 'required|email|unique:clientes',
+            'telefono' => 'required',
+            'direccion' => 'required',
+        ]);
+
+        $cliente = new Cliente();
+        $cliente->nombre = $request->nombre;
+        $cliente->correo = $request->correo;
+        $cliente->telefono = $request->telefono;
+        $cliente->direccion = $request->direccion;
+        $cliente->save();
+        return $cliente;
     }
+
 
     /**
      * Display the specified resource.
@@ -33,16 +47,48 @@ class ClienteController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, $id)
     {
-        //
+        $cliente = Cliente::find($id);
+    
+        if (!$cliente) {
+            return response()->json([
+                'message' => 'no se pudo realizar correctamente la operacion.'
+            ], 404);
+        }
+    
+        // Validar los datos. Si la validación falla, automáticamente se devuelve un error 422 y un mensaje de error.
+        $request->validate([
+            'nombre' => 'required',
+            'correo' => 'required|email|unique:clientes',
+            'telefono' => 'required',
+            'direccion' => 'required',
+        ]);
+    
+        // Actualizar el cliente con los nuevos datos.
+        $cliente->nombre = $request->nombre;
+        $cliente->correo = $request->correo;
+        $cliente->telefono = $request->telefono;
+        $cliente->direccion = $request->direccion;
+        $cliente->update(); // Puedes usar save() en lugar de update() si prefieres.
+    
+        return $cliente;
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy($id)
     {
-        //
+        $cliente = Cliente::find($id);
+
+        if (!$cliente) {
+            return response()->json([
+                'message' => 'no se pudo realizar correctamente la operacion'
+            ], 404);
+        }
+
+        $cliente->delete();
+        return response()->noContent();
     }
 }
